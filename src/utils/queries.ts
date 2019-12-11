162c789a7800a486dca603585461eb37dea513cb
Cristina Saez <csaez@itteria.cat>
2019-12-11 08:44:23 +0100

    query fixed

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -1,7 +1,7 @@
 export const SELECT_ALL = 'SELECT * FROM sites';
 // export const SELECT_ALL_SORTED ="SELECT * FROM sites ORDER BY watched, chapter_date DESC";
 export const SELECT_ALL_SORTED =
-  'SELECT * FROM sites ORDER BY watched,  FORMAT(chapter_last_published, 0) - FORMAT(chapter_last_readed, 0) DESC';
+  'SELECT * FROM sites ORDER BY watched,  FORMAT(chapter_last_published, 0) - FORMAT(chapter_last_read, 0) DESC';
 export const UPDATE_SITE =
   'UPDATE sites SET last_update=?, chapter_count=?, chapter_date=?, watched=?, chapter_last_read=?, chapter_last_published=?, status=?, favorite=?, archived=? WHERE id=?';
 export const SELECT_BY_ID = 'SELECT * FROM sites WHERE id = ?';
